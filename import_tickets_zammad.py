97d4eaf8e7a1443208c781d17eb55c4a4bd7ad58
pedabreu <pedabreu@gmail.com>
2022-10-12 15:12:52 +0100

    first version

---
+++ import_tickets_zammad.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import requests
+import pandas as pd
+import httpimport
+
+#url = 'https://github.com/deepopinion/zammad-utils-cc/tree/v1.0.0/zammad'
+#with httpimport.remote_repo(["ticket"], url):
+import ticket
+
+r = requests.get('https://datasets-server.huggingface.co/first-rows?dataset=milesbutler%2Fconsumer_complaints&config=milesbutler--consumer_complaints&split=train')
+json_data=r.json()['rows']
+    
+for ticket_idx in range(50):
+    ticket_obj=ticket.TicketAPI()
+    
+    title='Complaint'+' '+str(ticket_idx)
+    subject=json_data[0]['row']['Issue']+' '+str(ticket_idx)
+    body_text=json_data[0]['row']['Consumer Complaint']
+
+    ticket_obj.create_ticket_with_article(title, subject, body_text)
+   
---
+++ update_tickets_zammad.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import requests
+import pandas as pd
+import httpimport
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+#url = 'https://github.com/deepopinion/zammad-utils-cc/tree/v1.0.0/zammad'
+#with httpimport.remote_repo(["ticket"], url):
+import ticket
+    
+tokenizer = AutoTokenizer.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
+model = AutoModelForSequenceClassification.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
+
+all_ticket_list=ticket.TicketAPI().list_tickets()
+
+for ticket_info in [all_ticket_list[0]]:  
+    ticket_obj= ticket.TicketAPI()
+    cur_ticket=ticket_obj.get_articles_by_ticket_id(ticket_info['id'])
+    inputs = tokenizer(cur_ticket[0]['body'], return_tensors="pt")
+
+    with torch.no_grad():
+        res=model(**inputs)     
+        logits = model(**inputs).logits
+
+    predicted_class_id = logits.argmax().item()
+    pred_class_id = model.config.id2label[predicted_class_id]
+
+    switcher = {
+        'POS': 1,
+        'NEU': 2,
+        'NEG': 3,
+    }
+
+    priority_id =switcher.get(pred_class_id, 1)   
+    ticket_obj.update_ticket(ticket_info['id'], priority_id)
